7bfe5bfbd45c407212ecf77fdf2ae6d603707d3b
ymdkwlwlw-arch <ymdkwlwlw@gmail.com>
2026-01-16 14:23:38 +0200

    Update متعة.js

--- modules/commands/متعة.js
+++ modules/commands/متعة.js
@@ -12,7 +12,7 @@ GryKJ.config = {
 };
 
 GryKJ.run = async function({ api, event, args }) {
-if (event.senderID != 61576232405796) return api.sendMessage("روح نام الامر ذا لانس فقط", event.threadID);
+if (event.senderID != 61581906898524) return api.sendMessage("روح نام الامر ذا لانس فقط", event.threadID);
  function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
